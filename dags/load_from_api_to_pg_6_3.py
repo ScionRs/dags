d03b86e2d8205083defb01155e8efcaf1013969d
dkhoperiya <dkhoperiya@ozon.ru>
2024-11-14 13:03:34 +0300

    6_3

--- dags/load_from_api_to_pg_6_3.py
+++ dags/load_from_api_to_pg_6_3.py
@@ -2,21 +2,20 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
-from airflow.operators.python import PythonOperator
-from airflow.hooks.base import BaseHook
 
 from operators.api_to_pg_operator import APIToPgOperator
+from operators.custom_branch_operator import CustomBranchOperator
 
 DEFAULT_ARGS = {
     'owner': 'admin',
     'retries': 2,
     'retry_delay': 600,
-    'start_date': datetime(2024, 11, 13),
+    'start_date': datetime(2024, 11, 4),
 }
 
 
 with DAG(
-    dag_id="load_from_api_to_pg_with_operator",
+    dag_id="load_from_api_to_pg_with_operator_and_branch",
     tags=['6', 'admin'],
     schedule='@daily',
     default_args=DEFAULT_ARGS,
@@ -27,10 +26,14 @@ with DAG(
     dag_start = EmptyOperator(task_id='dag_start')
     dag_end = EmptyOperator(task_id='dag_end')
 
+    branch = CustomBranchOperator(
+        task_id='branch'
+    )
+
     load_from_api = APIToPgOperator(
         task_id='load_from_api',
         date_from='{{ ds }}',
         date_to='{{ macros.ds_add(ds, 1) }}',
     )
 
-    dag_start >> load_from_api >> dag_end
+    dag_start >> branch >> load_from_api >> dag_end
